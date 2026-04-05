a8e08784661d01f2ddb23159828b736e53c99e3b
Alexandr Lewandowski <aolewandowski94@gmail.com>
2026-04-05 19:07:30 +0300

    check and rework

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,38 @@
-// @todo: Темплейт карточки
+const placesList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+function handleDelete(e) {
+  const card = e.currentTarget.closest('li');
+  if (card) {
+    card.remove();
+  } else {
+    console.error('li не найден');
+  }
+}
 
-// @todo: Функция создания карточки
+function createCard(cardData, handleDelete) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardTitle = cardElement.querySelector('.card__title');
+  const cardImage = cardElement.querySelector('.card__image');
+  const likeButton = cardElement.querySelector('.card__like-button');
+  const deleteButton = cardElement.querySelector('.card__delete-button');
 
-// @todo: Функция удаления карточки
+  cardTitle.textContent = cardData.name;
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
 
-// @todo: Вывести карточки на страницу
+  likeButton.addEventListener('click', () => {
+    likeButton.classList.toggle('card__like-button_is-active');
+  });
+
+  if (deleteButton) {
+    deleteButton.addEventListener('click', handleDelete);
+  }
+
+  return cardElement;
+}
+
+initialCards.forEach((cardData) => {
+  const cardElement = createCard(cardData, handleDelete);
+  placesList.append(cardElement);
+});
